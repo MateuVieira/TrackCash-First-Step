818e53687871a80318007e33a67b1ae8f7e52fae
Mateus Vieira <mateus17v@gmail.com>
2020-10-15 17:56:25 -0300

    Update - send login and password values

--- firstStep.py
+++ firstStep.py
@@ -8,12 +8,26 @@ from selenium.webdriver.chrome.options import Options
 # Url to login 
 URL = 'https://marketplace.integracommerce.com.br/Account/Login'
 
+# Test credentials - provided by TrackCash
+#credenciais1
+LOGIN='desenvolvimento@comprenet.com.br'
+PASSWORD='tecnol@gia2@17'
+
 # Set selenium options
 options = Options()
 # options.headless = True
 driver = webdriver.Chrome(options=options)
 
 driver.get(URL)
+
+# Delay of 10 sec to load page
+time.sleep(10)
+
+fieldLogin = driver.find_element_by_xpath('//*[@id="username"]')
+fieldLogin.send_keys(LOGIN)
+
+fieldPassword = driver.find_element_by_xpath('//*[@id="password"]')
+fieldPassword.send_keys(PASSWORD)
 
 # End browser
 # driver.quit()
