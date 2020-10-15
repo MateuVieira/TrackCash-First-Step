4d5987080fb6ff1d5744c9c7daa42b944503a927
Mateus Vieira <mateus17v@gmail.com>
2020-10-15 17:43:41 -0300

    Update - set selenium options to use Chrome

--- firstStep.py
+++ firstStep.py
@@ -3,5 +3,17 @@ import requests
 import pandas as pd
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.chrome.options import Options
 
+# Url to login 
+URL = 'https://marketplace.integracommerce.com.br/Account/Login'
+
+# Set selenium options
+options = Options()
+# options.headless = True
+driver = webdriver.Chrome(options=options)
+
+driver.get(URL)
+
+# End browser
+# driver.quit()
