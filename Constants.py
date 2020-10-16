22b020846b0d744357ccec9b948b1b1700dfd6d1
Mateus Vieira <mateus17v@gmail.com>
2020-10-15 23:40:06 -0300

    Add Constants.py

---
+++ Constants.py
@@ -0,0 +1,17 @@
+# Url to login 
+URL = 'https://marketplace.integracommerce.com.br/Account/Login'
+
+# Url to take data
+URL_DATA = 'https://cdn.integracommerce.com.br/site/production/index.html#/Order'
+
+# Test credentials - provided by TrackCash
+#credenciais1
+LOGIN='desenvolvimento@comprenet.com.br'
+PASSWORD='tecnol@gia2@17'
+
+# Info to search data for a period of time
+START = '01/01/2018'
+END = '01/02/2018'
+
+# Arry with the names of the months in pt-Br
+arrMonths = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro']
